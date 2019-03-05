bd36e3f32dccaf343dc5d6fe9ef2d2c92193fe72
Bayley Wang <wang9xb17@gmail.com>
2019-03-04 23:14:20 -0500

    skydome

--- main.cpp
+++ main.cpp
@@ -10,6 +10,11 @@
 #include "ray_utils.h"
 #include "bmp.h"
 #include "bmpc.h"
+#include "brdf.h"
+
+brdf_t brdfs[64];
+vec3f base_colors[64];
+vec3f * hdri;
 
 int main(int argc, char ** argv) {
 	srand(time(0));
@@ -18,7 +23,22 @@ int main(int argc, char ** argv) {
 	RTCScene scene = rtcNewScene(device);
 
 	load_obj(device, scene, (char*)"models/teapot.obj", 0);
+	brdfs[0] = brdf_lambert;
+	base_colors[0] = {1.f, 1.f, 1.f};
+
 	rtcCommitScene(scene);
+
+	int hdri_w = 1440, hdri_h = 1440;
+	unsigned char *h_red, *h_green, *h_blue;
+	h_red = (unsigned char*)malloc(hdri_h * hdri_w);
+	h_green = (unsigned char*)malloc(hdri_h * hdri_w);
+	h_blue = (unsigned char*)malloc(hdri_h * hdri_w);
+	hdri = (vec3f *)malloc(hdri_h * hdri_w * sizeof(vec3f));
+	read_bmp(h_red, h_green, h_blue, hdri_w, hdri_h, (char*)"textures/bliss.bmp");
+	for (int i = 0; i < hdri_w * hdri_h; i++) {
+		hdri[i] = {(float)h_red[i] / 255.f, (float)h_green[i] / 255.f, (float)h_blue[i] / 255.f};
+	}
+	free(h_red); free(h_green); free(h_blue);
 
 	BMPC output(1000, 1000);
 
@@ -32,8 +52,11 @@ int main(int argc, char ** argv) {
 	RTCIntersectContext context;
 	rtcInitIntersectContext(&context);
 
-	vec3f hit_p, hit_n, last_dir;
+	vec3f hit_p, hit_n, last_dir, uv;
+	int last_id;
+	float backside;
 	vec3f g_illum;
+
 	for (int row = 0; row < output.width; row++) {
 		for (int col = 0; col < output.width; col++) {
 			rh.ray.tnear = 0.01f; rh.ray.tfar = FLT_MAX;
@@ -43,34 +66,45 @@ int main(int argc, char ** argv) {
 			set_dir(&rh, cam.lookat(row, col));
 
 			rtcIntersect1(scene, &context, &rh);
+			last_id = rh.hit.geomID;
 
-			if (rh.hit.geomID == -1) {
-				output.set_px(row, col, 1.f, 1.f, 1.f);
+			if (last_id == -1) {
+				//output.set_px(row, col, 1.f, 1.f, 1.f);
+				uv = intersect_hdri(&rh, 25.f);
+				int u = (int)(hdri_w * uv.x);
+				int v = (int)(hdri_h * uv.y);
+				output.set_px(row, col, hdri[v * hdri_w + u].x, hdri[v * hdri_w + u].y, hdri[v * hdri_w + v].z);
 				continue;
 			} 
 
 			hit_p = eval_ray(&rh, rh.ray.tfar);
 			hit_n = {rh.hit.Ng_x, rh.hit.Ng_y, rh.hit.Ng_z};
 			hit_n.normalize();
+			last_dir = {rh.ray.dir_x, rh.ray.dir_y, rh.ray.dir_z};
+			backside = last_dir.dot(hit_n) > 0.f ? -1.f : 1.f;
 
 			g_illum = {0.f, 0.f, 0.f};
 
 			for (int sample = 0; sample < 128; sample++) {
-				float backside = last_dir.dot(hit_n) > 0.f ? -1.f : 1.f;
 				vec3f out_dir = random_dir(hit_n, backside);
-				float cos_g = backside * hit_n.dot(out_dir);
 
+				float cos_g = backside * hit_n.dot(out_dir);
+				float pdf = brdfs[last_id](0.f, 0.f, 0.f, 0.f);
 				
 				rh.ray.tnear = 0.01f; rh.ray.tfar = FLT_MAX;
 				rh.hit.instID[0] = -1; rh.hit.geomID = -1;
+
 				set_org(&rh, hit_p);
 				set_dir(&rh, out_dir);
 
 				rtcIntersect1(scene, &context, &rh);
 				
 				if (rh.hit.geomID == -1) {
-					vec3f emit = {1.f, 1.f, 1.f};
-					g_illum += emit * cos_g;
+					uv = intersect_hdri(&rh, 25.f);
+					int u = (int)(hdri_w * uv.x);
+					int v = (int)(hdri_h * uv.y);
+					vec3f emit = {hdri[v * hdri_w + u].x, hdri[v * hdri_w + u].y, hdri[v * hdri_w + v].z};
+					g_illum += emit * base_colors[last_id] * cos_g * pdf;
 				}
 			}
 			g_illum /= 128.f;
--- ray_utils.h
+++ ray_utils.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <embree3/rtcore.h>
+#include <math.h>
 
 #include "geom.h"
 
@@ -53,6 +54,34 @@ inline vec3f eval_ray(RTCRayHit * rh, float t) {
   result.y = rh->ray.org_y + t * rh->ray.dir_y;
   result.z = rh->ray.org_z + t * rh->ray.dir_z;
   return result;
+}
+
+inline vec3f intersect_hdri(RTCRayHit * rh, float r) {
+	float ox = rh->ray.org_x; float oy = rh->ray.org_y; float oz = rh->ray.org_z;
+	float dx = rh->ray.dir_x; float dy = rh->ray.dir_y; float dz = rh->ray.dir_z;
+
+	float a = dx * dx + dy * dy + dz * dz;
+	float b = 2.f * ox * dx + 2.f * oy * dy + 2.f * oz * dz;
+	float c = ox * ox + oy * oy + oz * oz - r * r;
+	float disc = b * b - 4 * a * c;
+	
+	vec3f result = {0.f, 0.f, 0.f};
+	if (disc < 0) return result; //should not happen!
+
+	float sdisc = sqrtf(disc);
+	float t;
+	t = (-b - sdisc) / (2.f * a);
+	if (t < 0.f) t = (-b + sdisc) / (2.f * a);
+	if (t < 0.f) return result; //should not happen!
+
+	vec3f p = eval_ray(rh, t);
+	float theta, phi;
+	theta = acos(p.z / r);
+	phi = atan2(p.x, p.y) + M_PI;
+
+	result.x = phi / (2.f * M_PI);
+	result.y = theta / M_PI;
+	return result;
 }
 
 #endif
