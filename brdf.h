321abfec4bba5af1619e60321f5b7bbb2f50b045
Bayley Wang <wang9xb17@gmail.com>
2019-03-07 14:54:56 -0500

    replaced all brdf's with PrincipledBRDF class

--- brdf.h
+++ brdf.h
@@ -3,14 +3,6 @@
 
 #include "geom.h"
 
-typedef vec3f (*brdf_t)(float, float, float, float, float, float);
-
-//uniform BRDF
-vec3f brdf_lambert(float ci, float co, float th, float td, float ph, float pd);
-
-//WDAS principled BRDF
-vec3f brdf_principled(float ci, float co, float th, float td, float ph, float pd);
-
 //Principled BRDF object (with color)
 class PrincipledBRDF {
 public:
